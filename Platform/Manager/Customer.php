3043d6d4b3e57e0025088e8548c2d036eb7c21d3
John Sparwasser <johnspar1@gmail.com>
2017-06-14 10:04:13 -0400

    More debugging stuff

--- Platform/Manager/Customer.php
+++ Platform/Manager/Customer.php
@@ -66,7 +66,7 @@ class Customer
             $customer = $this->customerRepository->get($customerEmail, $websiteId);
             $platformCustomer = $this->createPlatformCustomer($customer, $websiteId);
         } else {
-            file_put_contents("/var/www/john-m2-1-6.spr0.com/var/log/test.log", SubGenerateCallTrace(), FILE_APPEND | LOCK_EX);
+            file_put_contents("/var/www/john-m2-1-6.spr0.com/var/log/test.log", "\n\n---------------\n\n" . SubGenerateCallTrace() . "\n\n---------------\n\n", FILE_APPEND | LOCK_EX);
             throw new NoSuchEntityException(__('Platform customer is not found.'));
         }
 
