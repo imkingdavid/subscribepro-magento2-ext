5521ce763984d720e581c763df2e27319d811fea
John Sparwasser <johnspar1@gmail.com>
2017-06-14 09:59:58 -0400

    Test

--- Platform/Manager/Customer.php
+++ Platform/Manager/Customer.php
@@ -93,3 +93,22 @@ class Customer
         return $platformCustomer;
     }
 }
+
+function SubGenerateCallTrace()
+{
+    $e = new Exception();
+    $trace = explode("\n", $e->getTraceAsString());
+    // reverse array to make steps line up chronologically
+    $trace = array_reverse($trace);
+    array_shift($trace); // remove {main}
+    array_pop($trace); // remove call to this method
+    $length = count($trace);
+    $result = array();
+
+    for ($i = 0; $i < $length; $i++)
+    {
+        $result[] = ($i + 1)  . ')' . substr($trace[$i], strpos($trace[$i], ' ')); // replace '#someNum' with '$i)', set the right ordering
+    }
+
+    return "\t" . implode("\n\t", $result);
+}
